01aebacfc3d7b8666798881b35a3c2de29fd28c5
Michael <micwesley@gmail.com>
2015-02-26 22:58:29 -0300

    Motorista version 26_02

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-function controller($scope) {
+function controller($scope, $timeout) {
    $scope.lista = [];
 
    $scope.add = function (pessoa, id) {
@@ -8,24 +8,21 @@ function controller($scope) {
 		$scope.pessoa.id = $scope.lista.length;
     	console.log($scope.pessoa.id);
         $scope.pessoa.select=false;
-    };
-    	
+    };	
 
     $scope.reset = function (){
 		$scope.pessoa = {};
-
 	};
 	 
     
     $scope.remove = function($index) {
     	$scope.lista.splice($index, 1);
     };
- 
 
     function random(){
         var rand = Math.random(10) * Number($scope.lista.length);
         var inteiro = Math.round(rand);
-        console.log(inteiro);
+        //console.log(inteiro);
         if (inteiro == 0 && $scope.lista.length != 0){
             random();
         }
@@ -37,45 +34,72 @@ function controller($scope) {
         var i;
         var escolhido;
         var but;
-        
+        var achou;
+        var timer = $scope.lista.length * 200;
+        but = document.querySelectorAll("button");
 
         for(i=1;i<=$scope.lista.length;i++){
-            but = document.querySelectorAll("button");//.className = "pessoas ng-binding";
+            //.className = "pessoas ng-binding";
             but[i].style.backgroundColor = "#FFD700";
             but[i].style.color = "#CD0000";
         }
 
-        var achou = false;
+        achou = false;
 
         if($scope.lista.length > 1){
-            for(i=0;i<=$scope.lista.length-1;i++){
-                if(i == num){
-                    //console.log("if");
-                    $scope.escolhido = $scope.lista[i].nome;
-                    console.log($scope.lista[i]);
-                    //showSelect(num);
-                    but = document.querySelectorAll("button");//.className = "selecionado ng-binding";
-                    but[i+1].style.backgroundColor = "#CD0000";
-                    but[i+1].style.color = "#ffffff";
+            //$timeout(function (){
+                //for(i=1;i<$scope.lista.length*200;i++){    
+                    $scope.animation(but); //$timeout aqui?
+                //}
+            //}, 1000);
+            $timeout(function (){
+                console.log("rand");
+                for(i=0;i<=$scope.lista.length-1;i++){
+                
+                    if(i == num){
+                        $scope.escolhido = $scope.lista[i].nome;
+                        console.log($scope.lista[i]);
+                    //.className = "selecionado ng-binding";
+                        but[i+1].style.backgroundColor = "#CD0000";
+                        but[i+1].style.color = "#ffffff";
                     //console.log(but[i]);
-                    achou = true;
+                        achou = true;
+                    }
                 }
-            }
             if(achou == false){
                 $scope.aponta();
             }
             return num;
-            setTimeout($scope.aponta(),100000);
+            }, timer);
         }
         else{
             $scope.escolhido = $scope.lista[0].nome;
             console.log($scope.lista[0].nome);
-            but = document.querySelectorAll("button");//.className = "selecionado ng-binding";
             but[1].style.backgroundColor = "#CD0000";
             but[1].style.color = "#ffffff";
         }
+    };
 
-    }
+    $scope.animation = function (but){
+        var time = 100*$scope.lista.length;
+        //var anterior = 1;
+            
+        for(var i=1;i<=$scope.lista.length;i++){
+            console.log("piscou?");
+                
+            but[i].style.backgroundColor = "#CD0000";
+            but[i].style.color = "#ffffff";
 
+
+            setTimeout(function(){      
+                for(var j=1;j<=$scope.lista.length;j++){
+                    but[j].style.backgroundColor = "#FFD700";
+                    but[j].style.color = "#CD0000";
+                }
+                //anterior = i + 1;
+            }, time);
+        }
+
+    };
 
 };
